98abb3fdaef7065a91425180e5c55fbdf1cfea3f
Fergan Yalim <ferganyalim13@gmail.com>
2022-10-01 21:59:38 +0100

    Add median finding capability

--- median.py
+++ median.py
@@ -5,8 +5,13 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        numbers_length = len(numbers)
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+if(numbers_length % 2 == 0):
+    print(numbers[int(numbers_length/2)] + numbers[int(numbers_length/2) - 1])
+else:
+    print(numbers[int(numbers_length/2)])
